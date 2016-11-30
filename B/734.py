91ada02afa42080265b0f4bf9ef683336bcbd110
v3sk0o0 <vesko.r.zdravkov@gmail.com>
2016-11-30 22:51:13 +0200

    B

---
+++ B/734.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/python3.4
+k2, k3 , k5 , k6 = map( int, input().split() )
+minimal256=min(k6,k5,k2)
+sum=256*minimal256
+k2-=minimal256
+minimal32=min(k3,k2)
+sum+=minimal32 * 32
+
+print (sum)
+
+
+
+
+
+
+
