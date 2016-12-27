b0f1ed1f4d93b161e25eb14a7af1c0957764879c
v3sk0o0 <vesko.r.zdravkov@gmail.com>
2016-12-28 00:00:58 +0200

    A

---
+++ A/749x.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python3.4
+N=int(input())
+
+
+if N % 2 == 0:
+    times= N / 2
+    
+else:
+    times = (N-3)/2
+  
+result=list()
+for _ in range(0,int(times)):
+    result.append(2)
+
+if N % 2 != 0:
+    result.append(3)
+print (len(result))
+print (*result,sep=' ')
